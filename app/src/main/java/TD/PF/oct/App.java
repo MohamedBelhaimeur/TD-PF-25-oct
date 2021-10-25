0003558e7dd8f1e9e1143fb9ddf7547c34fb1cc6
Mohamed Belhaimeur <belhaim789@gmail.com>
2021-10-25 17:39:11 +0200

    exo3

--- app/src/main/java/TD/PF/oct/App.java
+++ app/src/main/java/TD/PF/oct/App.java
@@ -4,8 +4,14 @@
 package TD.PF.oct;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+
+
+import TD.PF.oct.universite.*;
 
 
 public class App {
@@ -84,8 +90,8 @@ Consumer : Consume une entrée et ne retourne rien Stockage Lambda expression : 
 Supplier : Générer une sortie Stockage Lambda expression : signature ()  T
  */
 
-}
-    
+
+    //Exo 2 q1)
     Predicate<Integer> tailleTropPetite =(s) -> s<100;
     Predicate<Integer> tailleTropGrande =(s) -> s>200;
     Predicate<Integer> tailleIcorrecte= tailleTropPetite.or(tailleTropGrande);
@@ -94,5 +100,34 @@ Supplier : Générer une sortie Stockage Lambda expression : signature ()  T
     Predicate<Double>poidcorrecte=poidtroplourd.negate();
     Predicate<Paire<Integer, Double>>accesautorise=
     		(Paire<Integer,Double>client) ->taillecorrecte.test(client.taille) && poidcorrecte.test(client.poid);
+    		
+    		
+    		Matiere m1 = new Matiere("MAT1");
+    		Matiere m2 = new Matiere("MAT2");
+    		UE ue1 = new UE("UE1", Map.of(m1, 2, m2, 2));
+    		Matiere m3 = new Matiere("MAT3");
+    		UE ue2 = new UE("UE2", Map.of(m3, 1));
+    		Annee a1 = new Annee(Set.of(ue1, ue2));
+    
+    		Etudiant e1 = new Etudiant("39001", "Alice", "Merveille",a1);
+    		e1.noter(m1, 12.0);
+    		e1.noter(m2, 14.0);
+    		e1.noter(m3, 10.0);
+    		System.out.println(e1);
+    		Etudiant e2 = new Etudiant("39002", "Bob", "Eponge", a1);
+    		e2.noter(m1, 14.0);
+    		e2.noter(m3, 14.0);
+    		Etudiant e3 = new Etudiant("39003", "Charles", "Chaplin", a1);
+    		e3.noter(m1, 18.0);
+    		e3.noter(m2, 5.0);
+    		e3.noter(m3, 14.0);
+    		
+    
+}
+    public static void afficheSi(String entete,Predicate<Etudiant> etudiant,Annee a,List<Etudiant>etudiants) {
+    	
+		 
+	 }
+   
     
 }
